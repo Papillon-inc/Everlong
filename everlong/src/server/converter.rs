1822160567284d976015d0c4ce68cfd61176e2a8
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-06-03 16:17:02 +0900

    tsファイルの作成

--- everlong/src/server/converter.rs
+++ everlong/src/server/converter.rs
@@ -1,8 +1,10 @@
 extern crate mpeg2ts;
 extern crate bytes;
 
+use std::fs;
+use std::io::BufWriter;
 use mpeg2ts::ts::{TsPacketReader, TsPacketWriter, ReadTsPacket, WriteTsPacket};
-use mpeg2ts::ts::{TsHeader, AdaptationField, TsPayload};
+use mpeg2ts::ts::{TsPacket, TsHeader, AdaptationField, TsPayload};
 use mpeg2ts::ts::{Pid, TransportScramblingControl, ContinuityCounter};
 use bytes::Bytes;
 
@@ -10,21 +12,19 @@ pub struct Converter;
 
 impl Converter {
     pub fn convert(data: &Bytes) {
-        println!("Converter is called!");
-        // println!("{:?}", *data);
-
-        let mut writer = TsPacketWriter::new(std::io::stdout());
-
-        //Result::unwrap(writer.write_ts_packet(data));
-    }
-
-    // This function is necessary for writing ts file.
-    pub fn make_ts_packet(data: &Bytes) {
-        let f = generate_adaptation_field();
+        let mut writer = TsPacketWriter::new(BufWriter::new(fs::File::create("ts/one.ts").unwrap()));
+        let packet = make_ts_packet(data);
+        Result::unwrap(writer.write_ts_packet(&packet));
     }
 }
 
-fn generate_adaptation_field() {
+// This function is necessary for writing ts file.
+fn make_ts_packet(data: &Bytes) -> TsPacket {
+    let f = generate_adaptation_field();
+    f
+}
+
+fn generate_adaptation_field() -> TsPacket {
     let header = TsHeader {
         transport_error_indicator: false,
         transport_priority: false,
@@ -32,7 +32,6 @@ fn generate_adaptation_field() {
         transport_scrambling_control: TransportScramblingControl::NotScrambled,
         continuity_counter: ContinuityCounter::new(),
     };
-
     let field = AdaptationField {
         discontinuity_indicator: false,
         random_access_indicator: false,
@@ -43,6 +42,12 @@ fn generate_adaptation_field() {
         transport_private_data: Vec::new(),
         extension: None,
     };
-
     let payload: Option<TsPayload> = None;
+
+    let packet = TsPacket {
+        header: header,
+        adaptation_field: Some(field),
+        payload: payload,
+    };
+    packet
 }
