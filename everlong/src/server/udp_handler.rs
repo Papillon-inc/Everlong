167addba1d8b6d31d957a3e4a218b750e3152ffc
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-06-15 22:51:46 +0900

    作成されたファイル名をudpでhlsサーバーへ送信

--- everlong/src/server/udp_handler.rs
+++ everlong/src/server/udp_handler.rs
@@ -5,20 +5,19 @@ use std::fs;
 pub struct UdpHandler;
 
 impl UdpHandler {
-    pub fn handle(executed_connection_id: &usize) {
+    pub fn handle(_executed_connection_id: &usize) {
         let socket = UdpSocket::bind("127.0.0.1:54321").expect("failed to bind socket");
         socket.set_write_timeout(Some(Duration::from_secs(2))).unwrap();
 
-        loop {
-            let dir_name = format!("../ts/{}", executed_connection_id);
-            let dir_path = fs::read_dir(dir_name).unwrap();
-            let packet = format!("{:08?}", dir_path.count());
+        let dir_name = format!("../ts/{}", _executed_connection_id);
+        let dir_path = fs::read_dir(dir_name).unwrap();
+        let packet = format!("{:08?}", dir_path.count());
+        print!("{:?}", packet);
 
-            socket.send_to(packet.as_bytes(), "127.0.0.1:12345").expect("failed to send data");
+        socket.send_to(packet.as_bytes(), "127.0.0.1:12345").expect("failed to send data");
 
-            //let mut buffer = [0u8; 1024];
-            //socket.recv_from(&mut buffer).expect("failed to receive");
-            //print!("{}", str::from_utf8(&buffer).expect("failed to convert to String"));
-        }
+        let mut buffer = [0u8; 1024];
+        socket.recv_from(&mut buffer).expect("failed to receive");
+        //print!("{}", str::from_utf8(&buffer).expect("failed to convert to String"));
     }
 }
--- my_hero/src/main.rs
+++ my_hero/src/main.rs
@@ -5,18 +5,21 @@ fn main() {
     let port = "127.0.0.1:12345";
     let server_socket = UdpSocket::bind(port)
                            .expect("Could not bind socket");
+                    
+    println!("UDP Server started!");
+    println!("Listening on port: {}", port);
+    
     loop {
         let mut buf = [0u8; 1024];
         // クロージャの中に移動されるからクローンする。
         let client_socket = server_socket.try_clone().expect("failed to clone socket");
-        println!("UDP Server started!");
-        println!("Listening on port: {}", port);
 
         match server_socket.recv_from(&mut buf) {
             Ok((_, src)) => {
                 thread::spawn(move || {
-                    println!("handling data from {}", src);
+                    //println!("handling data from {}", src);
                     client_socket.send_to(&buf, src).expect("failed to send response");
+                    println!("Data: {:?}", &buf[..]);
                 });
             },
             Err(e) => {
